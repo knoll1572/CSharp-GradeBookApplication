da97e20606be87de98b919fbc8a73c932df9bfdc
knoll1572 <31300393+knoll1572@users.noreply.github.com>
2021-05-23 23:15:03 -0500

    GetLetterGrade methos

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +9,14 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            return base.GetLetterGrade(averageGrade);
+        }
+
     }
 }
